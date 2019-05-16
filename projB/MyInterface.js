f149d8a551cc8590e051f740f818be5833206b42
Andre Rocha <andrefmrocha@live.com.pt>
2019-05-16 15:42:00 +0100

    Added linter information

--- projB/MyInterface.js
+++ projB/MyInterface.js
@@ -2,20 +2,49 @@
 * MyInterface
 * @constructor
 */
+/* eslint-disable no-undef */
 class MyInterface extends CGFinterface {
-    constructor() {
+    // eslint-disable-next-line no-useless-constructor
+    constructor () {
         super();
     }
 
-    init(application) {
+    init (application) {
         // call CGFinterface init
         super.init(application);
         // init GUI. For more information on the methods, check:
         // http://workshop.chromeexperiments.com/examples/gui
         this.gui = new dat.GUI();
-        
-        var obj = this;
+
+        let obj = this;
+        this.model = {};
+        this.initKeys();
+
+        this.gui.add(this.scene, 'displayAxis').name('Display axis');
+
+        this.gui.add(this.scene, 'selectedDayState', this.scene.dayStates).name('Selected day state').onChange(this.scene.setDayState.bind(this.scene));
+
+        this.gui
+            .add(this.scene, 'textures')
+            .name('Enable Textures');
 
         return true;
+    }
+    initKeys () {
+        this.scene.gui = this;
+        this.processKeyboard = function () { };
+        this.model.activeKeys = {};
+    }
+
+    processKeyDown (event) {
+        this.model.activeKeys[event.code] = true;
+    };
+
+    processKeyUp (event) {
+        this.model.activeKeys[event.code] = false;
+    };
+
+    isKeyPressed (keyCode) {
+        return this.model.activeKeys[keyCode] || false;
     }
 }
--- projB/MyScene.js
+++ projB/MyScene.js
@@ -2,16 +2,19 @@
 * MyScene
 * @constructor
 */
+/* eslint-disable no-undef */
+
 class MyScene extends CGFscene {
-    constructor() {
+    // eslint-disable-next-line no-useless-constructor
+    constructor () {
         super();
     }
-    init(application) {
+    init (application) {
         super.init(application);
         this.initCameras();
         this.initLights();
 
-        //Background color
+        // Background color
         this.gl.clearColor(0.0, 0.0, 0.0, 1.0);
 
         this.gl.clearDepth(100.0);
@@ -21,32 +24,32 @@ class MyScene extends CGFscene {
         this.enableTextures(true);
         this.setUpdatePeriod(50);
 
-        //Initialize scene objects
+        // Initialize scene objects
         this.axis = new CGFaxis(this);
         this.plane = new Plane(this, 32);
 
-        //Objects connected to MyInterface
+        // Objects connected to MyInterface
     }
-    initLights() {
+    initLights () {
         this.lights[0].setPosition(15, 2, 5, 1);
         this.lights[0].setDiffuse(1.0, 1.0, 1.0, 1.0);
         this.lights[0].enable();
         this.lights[0].update();
     }
-    initCameras() {
+    initCameras () {
         this.camera = new CGFcamera(0.4, 0.1, 500, vec3.fromValues(45, 45, 45), vec3.fromValues(0, 0, 0));
     }
-    setDefaultAppearance() {
+    setDefaultAppearance () {
         this.setAmbient(0.2, 0.4, 0.8, 1.0);
         this.setDiffuse(0.2, 0.4, 0.8, 1.0);
         this.setSpecular(0.2, 0.4, 0.8, 1.0);
         this.setShininess(10.0);
     }
-    update(t){
+    update (t) {
 
     }
 
-    display() {
+    display () {
         // ---- BEGIN Background, camera and axis setup
         // Clear image and depth buffer everytime we update the scene
         this.gl.viewport(0, 0, this.gl.canvas.width, this.gl.canvas.height);
@@ -60,12 +63,12 @@ class MyScene extends CGFscene {
         // Draw axis
         this.axis.display();
 
-        //Apply default appearance
+        // Apply default appearance
         this.setDefaultAppearance();
 
         // ---- BEGIN Primitive drawing section
         this.pushMatrix();
-        this.rotate(-0.5*Math.PI, 1, 0, 0);
+        this.rotate(-0.5 * Math.PI, 1, 0, 0);
         this.scale(60, 60, 1);
         this.plane.display();
         this.popMatrix();
